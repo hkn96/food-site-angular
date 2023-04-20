ef4d116bb3932256d9715008e8a21ed74ac6e12c
Hakan Ünlü <unluhak@outlook.com>
2023-04-20 17:14:08 +0200

    Food Dteails Card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FoodService } from 'src/app/services/food/food.service';
+import { Food } from 'src/app/shared/models/Food';
+
 
 
 @Component({
@@ -8,12 +10,14 @@ import { FoodService } from 'src/app/services/food/food.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-foods:String[] = []
+foods:Food[] = []
 
 
    constructor (private foodService:FoodService){}
 
    ngOnInit():void{
     this.foods = this.foodService.getAll()
+ 
+    
    }
 }
